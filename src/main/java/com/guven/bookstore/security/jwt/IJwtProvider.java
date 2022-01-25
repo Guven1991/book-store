b8ab92c699371e01c417d12206f74be5951235f8
Guven Ayvazoglu <gayvazoglu.29@gmail.com>
2022-01-25 10:46:54 +0300

    JwtAuthorizationFilter added

--- src/main/java/com/guven/bookstore/security/jwt/IJwtProvider.java
+++ src/main/java/com/guven/bookstore/security/jwt/IJwtProvider.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import javax.servlet.http.HttpServletRequest;
 
 public interface IJwtProvider {
+
     String generateToken(UserPrincipal auth);
 
     Authentication getAuthentication(HttpServletRequest request);
